0981907a4828ddefd28fe443d6af9c7125f63289
stijn@yoink.nl <stijn@yoink.nl>
2020-04-10 13:22:19 +0200

    added setup

---
+++ src/main/java/com/stijn/reversiservice/ReversiserviceApplication.java
@@ -0,0 +1,13 @@
+package com.stijn.reversiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReversiserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReversiserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stijn/reversiservice/ReversiserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stijn.reversiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReversiserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
